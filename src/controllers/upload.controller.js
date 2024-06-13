150c4b2aede971e791707498655013f7aec6639b
chandrababu-gowda <chandrababugowda3042003@gmail.com>
2024-06-13 16:01:12 +0530

    Feature: Generate remedy using Google Gemini

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -2,11 +2,17 @@ import { Image } from "../models/image.model.js";
 import { v4 as uuidv4 } from "uuid";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { config } from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 config();
 
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
+});
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+  model: process.env.GOOGLE_GEMINI_MODEL,
 });
 
 async function uploadImage(req, res) {
@@ -25,14 +31,22 @@ async function uploadImage(req, res) {
   await s3.send(command);
 
   Image.create(imageCreationData)
-    .then(() => {
+    .then(async () => {
       console.log(`Success: Image uploaded`);
-      res.json({ message: "Image uploaded" });
+      const remedy = await generateRemedy();
+      res.json({ message: "Image uploaded", remedy });
     })
     .catch((err) => {
       console.log(`Error: Unable to upload image\n${err}`);
       res.json({ message: "Unable to upload image" });
     });
+}
+
+async function generateRemedy() {
+  const prompt = `Tell me about Pagani Utopia in 20 words`;
+  const result = await model.generateContent(prompt);
+
+  return result.response.text();
 }
 
 export { uploadImage };
