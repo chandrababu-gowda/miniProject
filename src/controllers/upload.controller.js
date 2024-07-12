a27ba208430bd989e10783c72f3af819e0072228
chandrababu-gowda <chandrababugowda3042003@gmail.com>
2024-07-12 22:59:49 +0530

    Improve: Improve the remedy/solution generation prompt

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -56,7 +56,14 @@ async function uploadImage(req, res) {
 }
 
 async function generateRemedy(data) {
-  const prompt = `Give me the remedy for the nutrients deffeciency in banana leaf ${data}`;
+  const prompt = `"Potassium Deficiency: Foliar application of Potassium Nitrate (13:0:45) @ 5g per litre of water or Sulphate of Potash @ 5 g per litre.
+  Sulphur deficiency: Spray of elemental sulphur @ 5 g per litre or spray of potassium sulphate @ 5 g per litre recommended or foliar spray of Banana special in Banana @ 5g per litre is recommended.
+  Calcium deficiency: Soil application of  Calcium carbonate (Lime ) @ 50 g per plant  recommended. 
+  Magnesium Deficiency: Application of Magnesium Sulphate @ 25g per plant or Foliar application of Banana special @ 5 g per litre recommended .
+  Manganese Deficiency: Spraying of 0.5 % Manganese sulphate or spray of Bananaspecial @ 5 g per litre recommended. 
+  Zinc deficiency: Spray of 0.5 % zinc sulphate or spray of  banana special @ 5 g per litre recommended. 
+  Boron Deficiency: Soil application of Borax @ 25 g per plant or foliar application of 0.1 % boron(Solubore) or foliar application of Banana special @ 5g per litre recommended.
+  Iron deficiency: foliar spray of 0.5 % Iron sulphate with 1 % urea recommended   or Spray of Banana special @ 5 g per litre is recommended." Use the given data and generate the remedy/solution if there is any nutritional defeciency in the given plant data ${data}`;
   const result = await model.generateContent(prompt);
 
   return result.response.text();
