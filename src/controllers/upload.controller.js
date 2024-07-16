5306360295008d63045265d28aa2de441777c831
chandrababu-gowda <chandrababugowda3042003@gmail.com>
2024-07-16 15:55:01 +0530

    Improvement: Add some features

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { config } from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import axios from 'axios'
 
 config();
 
@@ -19,35 +20,48 @@ async function uploadImage(req, res) {
   const imageName = uuidv4();
   const imageCreationData = { name: imageName, date: new Date() };
 
+  // const file = req.file
+
+  // console.log("hello");
+  // console.log(file);
+
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: imageName,
     Body: req.file.buffer,
-    ContentType: req.file.mimeType,
+    ContentType: req.file.mimetype,
   };
 
+  // console.log(params)
   const command = new PutObjectCommand(params);
-  await s3.send(command);
+
+  try {
+    await s3.send(command);
+    //console.log(res)
+  }
+  catch (e) {
+    console.log("failed to send to s3", e)
+  }
 
   Image.create(imageCreationData)
     .then(async () => {
-      console.log(`Image add to database`);
-      fetch("http://127.0.0.1:5000/", {
-        method: "POST",
-        headers: {
-          ContentType: "application/json",
-        },
-        body: JSON.stringify({ name: imageName }),
-      })
+      // fetch("http://127.0.0.1:5000/", {
+      //   method: 'POST',
+      //   headers: {
+      //     ContentType: "application/json",
+      //   },
+      //   body: { "name": imageName },
+
+      // })
+      await axios.post("http://127.0.0.1:5000/", { "name": imageName })
+
         .then(async (response) => {
-          console.log(response);
-          const searchCondition = { name: imageName };
-          const remedy = await generateRemedy(response);
-          const updateData = { nutrients: response, remedy };
-          await Image.findOneAndUpdate(searchCondition, updateData);
-          res.json({ message: "Image uploaded", response, remedy });
-        })
-        .catch((err) => {
+          console.log("Sent and returned from python server")
+          // console.log(response.data)
+          const remedy = await generateRemedy(response.data);
+          // console.log(remedy)
+          res.json({ message: "Image uploaded", response: response.data, remedy });
+        }).catch((err) => {
           console.log(`Error: Unable to send to python file`);
           res.json({ message: "Unable to analyze the image" });
         });
@@ -59,16 +73,34 @@ async function uploadImage(req, res) {
 }
 
 async function generateRemedy(data) {
-  const prompt = `"Potassium Deficiency: Foliar application of Potassium Nitrate (13:0:45) @ 5g per litre of water or Sulphate of Potash @ 5 g per litre.
-  Sulphur deficiency: Spray of elemental sulphur @ 5 g per litre or spray of potassium sulphate @ 5 g per litre recommended or foliar spray of Banana special in Banana @ 5g per litre is recommended.
-  Calcium deficiency: Soil application of  Calcium carbonate (Lime ) @ 50 g per plant  recommended. 
-  Magnesium Deficiency: Application of Magnesium Sulphate @ 25g per plant or Foliar application of Banana special @ 5 g per litre recommended .
-  Manganese Deficiency: Spraying of 0.5 % Manganese sulphate or spray of Bananaspecial @ 5 g per litre recommended. 
-  Zinc deficiency: Spray of 0.5 % zinc sulphate or spray of  banana special @ 5 g per litre recommended. 
-  Boron Deficiency: Soil application of Borax @ 25 g per plant or foliar application of 0.1 % boron(Solubore) or foliar application of Banana special @ 5g per litre recommended.
-  Iron deficiency: foliar spray of 0.5 % Iron sulphate with 1 % urea recommended   or Spray of Banana special @ 5 g per litre is recommended." Use the given data and generate the remedy/solution if there is any nutritional defeciency in the given plant data ${data}. Give the solution in the plain text format.`;
-  const result = await model.generateContent(prompt);
+  console.log(data)
+  const prompt = `
+  For a banana plant, I have the following standard values and remedies for various nutrient deficiencies:
+  - Potassium Deficiency: Foliar application of Potassium Nitrate (13:0:45) @ 5g per litre of water or Sulphate of Potash @ 5 g per litre.
+  - Sulphur Deficiency: Spray of elemental sulphur @ 5 g per litre or spray of potassium sulphate @ 5 g per litre recommended or foliar spray of Banana special in Banana @ 5g per litre is recommended.
+  - Calcium Deficiency: Soil application of Calcium carbonate (Lime) @ 50 g per plant recommended.
+  - Magnesium Deficiency: Application of Magnesium Sulphate @ 25g per plant or Foliar application of Banana special @ 5 g per litre recommended.
+  - Manganese Deficiency: Spraying of 0.5% Manganese sulphate or spray of Banana special @ 5 g per litre recommended.
+  - Zinc Deficiency: Spray of 0.5% zinc sulphate or spray of banana special @ 5 g per litre recommended.
+  - Boron Deficiency: Soil application of Borax @ 25 g per plant or foliar application of 0.1% boron (Solubore) or foliar application of Banana special @ 5g per litre recommended.
+  - Iron Deficiency: Foliar spray of 0.5% Iron sulphate with 1% urea recommended or Spray of Banana special @ 5 g per litre is recommended.
+  
+  Using the given data, please generate a remedy/solution if there is any nutritional deficiency in the plant data provided below. The data is in the form of objects of objects, decode it if necessary, and generate the solution based on the standard remedies provided. Please give the solution in plain text format.
+  
+  Here is the data:
+  ${JSON.stringify(data)}
+  
+  - Boron Deficiency: ${data.Boron.Deficient}
+  - Iron Deficiency: ${data.Iron.Deficient}
+  - Calcium Deficiency: ${data.Calcium.Deficient}
+  - Potassium Deficiency: ${data.Potassium.Deficient}
+  
+  This data is in the form of objects. Now iterate through the object and generate the remedy. The remedy should be one among those I have given and very short.
+  `;
 
+
+  const result = await model.generateContent(prompt);
+  // console.log(result.response)
   return result.response.text();
 }
 
